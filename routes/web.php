f571b3a633ed7ab646be2c3cccd84252a72cd146
Ucitech Setia <ucitech13@gmail.com>
2024-11-11 00:02:50 +0700

    route lagi

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,7 @@ Route::get('/depan/getdesabyid/{kec_id}', [DepanController::class, 'getDesaByKec
 Route::get('/depan/getjurusanbyid/{pendidikan_id}', [DepanController::class, 'getJurusanByPendidikan'])->name('get-jurusan-bypendidikan');
 Route::post('/depan/akhir_daftar-akun', [DepanController::class, 'akhir_daftar_akun'])->name('akhir-daftar-akun');
 Route::post('/depan/akhir_daftar-akun-perush', [DepanController::class, 'akhir_daftar_akun_perush'])->name('akhir-daftar-akun-perush');
+Route::get('/depan/getkabkotabyid/{prov_id}', [DepanController::class, 'getKabkotaByProv'])->name('get-kabkota-byprov');
 
 
 
