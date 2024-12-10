8cd48024bfddea4d402d7547c55f6cd5f3aea2b5
Ucitech Setia <ucitech13@gmail.com>
2024-12-10 21:12:41 +0800

    web aja

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::get('/depan/login', [DepanController::class, 'login']);
 Route::get('/depan/register', [DepanController::class, 'register']);
 Route::get('depan/lowongan-kerja', [DepanController::class, 'lowongan_kerja'])->name('depan.lowongan-kerja');
 Route::get('/depan/lowongan-kerja-disabilitas', [DepanController::class, 'lowongan_kerja_disabilitas']);
+Route::get('/depan/lowongan-detail/{id}', [DepanController::class, 'lowongan_show'])->name('lowongan.show');
 Route::get('/depan/infografis', [DepanController::class, 'infografis']);
 Route::get('/depan/galeri', [DepanController::class, 'galeri']);
 Route::get('/depan/berita', [DepanController::class, 'berita']);
