14b457d8683eba680854870cce5c9051e82d2f08
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-02-12 10:59:43 +0700

    dashboard update

--- resources/views/backend/dashboard/index_penyedia.blade.php
+++ resources/views/backend/dashboard/index_penyedia.blade.php
@@ -109,12 +109,16 @@
                                 </div>
                             </div>
 
+                            
 
 
 
                         </div>
                         <!-- page statustic card end -->
                     </div>
+
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/6BXoUmI4G-Q?si=4S8Hal7hmyAfIHD-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 
 
                 </div>
