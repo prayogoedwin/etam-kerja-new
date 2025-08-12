443f6767be2af57fda504f47276069a247ae9bd5
Ucitech Setia <ucitech13@gmail.com>
2025-08-12 13:13:06 +0700

    verify false api

--- app/Http/Controllers/DashboardPimpinanController.php
+++ app/Http/Controllers/DashboardPimpinanController.php
@@ -9,21 +9,29 @@ class DashboardPimpinanController extends Controller
 {
     public function index()
     {
-        $resPencari = Http::get(url('/api/dashboard/pencari'))->json()['data'];
+        // $resPencari = Http::get(url('/api/dashboard/pencari'))->json()['data'];
+        $resPencari = Http::withOptions(['verify' => false])->get(url('/api/dashboard/pencari'))->json()['data'];
         // echo $resPencari->body();
 
-        $resLamaran = Http::get(url('/api/dashboard/lamaran'))->json()['data'];
+        // $resLamaran = Http::get(url('/api/dashboard/lamaran'))->json()['data'];
+        $resLamaran = Http::withOptions(['verify' => false])->get(url('/api/dashboard/lamaran'))->json()['data'];
         // echo $resLamaran->body();
 
-        $resPerusahaan = Http::get(url('/api/dashboard/perusahaan'))->json()['data'];
+        // $resPerusahaan = Http::get(url('/api/dashboard/perusahaan'))->json()['data'];
+        $resPerusahaan = Http::withOptions(['verify' => false])->get(url('/api/dashboard/perusahaan'))->json()['data'];
 
-        $resLowongan = Http::get(url('/api/dashboard/lowongan'))->json()['data'];
+        // $resLowongan = Http::get(url('/api/dashboard/lowongan'))->json()['data'];
+        $resLowongan = Http::withOptions(['verify' => false])->get(url('/api/dashboard/lowongan'))->json()['data'];
 
-        $resPenempatan = Http::get(url('/api/dashboard/penempatan'))->json()['data'];
+        // $resPenempatan = Http::get(url('/api/dashboard/penempatan'))->json()['data'];
+        $resPenempatan = Http::withOptions(['verify' => false])->get(url('/api/dashboard/penempatan'))->json()['data'];
 
-        $resTopPendidikan = Http::get(url('/api/dashboard/top_pendidikan'))->json()['data'];
-        $resTopJurusan = Http::get(url('/api/dashboard/top_jurusan'))->json()['data'];
-        $resTopSektor = Http::get(url('/api/dashboard/top_sektor'))->json()['data'];
+        // $resTopPendidikan = Http::get(url('/api/dashboard/top_pendidikan'))->json()['data'];
+        $resTopPendidikan = Http::withOptions(['verify' => false])->get(url('/api/dashboard/top_pendidikan'))->json()['data'];
+        // $resTopJurusan = Http::get(url('/api/dashboard/top_jurusan'))->json()['data'];
+        $resTopJurusan = Http::withOptions(['verify' => false])->get(url('/api/dashboard/top_jurusan'))->json()['data'];
+        // $resTopSektor = Http::get(url('/api/dashboard/top_sektor'))->json()['data'];
+        $resTopSektor = Http::withOptions(['verify' => false])->get(url('/api/dashboard/top_sektor'))->json()['data'];
 
         return view('backend.dashboardpimpinan.index',
         compact('resPencari','resLamaran', 'resPerusahaan', 'resLowongan', 'resPenempatan', 'resTopPendidikan', 'resTopJurusan', 'resTopSektor'));
