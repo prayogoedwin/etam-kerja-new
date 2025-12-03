6d9311a8b0f191f285e2eb7018175f97a136e661
Ucitech Setia <ucitech13@gmail.com>
2025-12-03 21:30:41 +0800

    sidebar bkk adminprov / superadmin

--- resources/views/backend/template/sidebar.blade.php
+++ resources/views/backend/template/sidebar.blade.php
@@ -70,16 +70,18 @@
                     </ul>
                 </li>
 
-                 <li class="nav-item"><a href="{{ route('jobfair.index') }}" class="nav-link "><span
+                <li class="nav-item"><a href="{{ route('jobfair.index') }}" class="nav-link "><span
                             class="pcoded-micon"><i class="feather icon-briefcase"></i></span><span
                             class="pcoded-mtext">Job Fair</span></a></li>
-
-
 
 
                 <li class="nav-item"><a href="{{ route('lowongan.admin.index') }}" class="nav-link "><span
                             class="pcoded-micon"><i class="feather icon-briefcase"></i></span><span
                             class="pcoded-mtext">Lowongan</span></a></li>
+
+                <li class="nav-item"><a href="{{ route('lowonganbkk.admin.index') }}" class="nav-link "><span
+                            class="pcoded-micon"><i class="feather icon-briefcase"></i></span><span
+                            class="pcoded-mtext">Lowongan BKK</span></a></li>
 
                 <li class="nav-item"><a href="{{ route('penempatan.admin.index') }}" class="nav-link "><span
                             class="pcoded-micon"><i class="fa fa-archive"></i></span><span
