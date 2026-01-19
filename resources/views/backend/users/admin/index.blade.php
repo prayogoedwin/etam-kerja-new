a8f98879146ca0f5a07e1302b1b6e9c87326bfa1
Edwin <prayogo.edwin@gmail.com>
2026-01-19 16:44:41 +0700

    update view modal

--- resources/views/backend/users/admin/index.blade.php
+++ resources/views/backend/users/admin/index.blade.php
@@ -175,7 +175,7 @@
             <div class="modal-dialog modal-lg">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="modalEditLabel">Edit Admin A</h5>
+                        <h5 class="modal-title" id="modalEditLabel">Edit Admin</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
